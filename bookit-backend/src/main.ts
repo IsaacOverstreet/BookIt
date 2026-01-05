e1539e13ed599afff689072db019482cdae5d0dd
Isaac <morakinyorisaac@yahoo.com>
2026-01-05 16:38:46 +0100

    fix cors

--- bookit-backend/src/main.ts
+++ bookit-backend/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // your frontend
+    origin: process.env.FRONTEND_URL, // your frontend
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
